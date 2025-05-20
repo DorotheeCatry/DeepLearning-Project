a0a880828f730152215e840dbf916c6e8c5907d1
ludivine <ludivine.raby@gmail.com>
2025-05-20 09:16:26 +0200

    Modification of the comments

--- cleaning/cleaning_data.py
+++ cleaning/cleaning_data.py
@@ -15,10 +15,10 @@ df = pd.read_csv(input_path)
 
 # Cleaning Tasks
 
-#Transformation of the TotalCharges column in float
+# Transformation of the TotalCharges column in float
 df["TotalCharges"] = df["TotalCharges"].str.strip().replace(",", ".", regex=True).replace("", np.nan).astype(float)
 
-#Transformation of all object columns in lowercase
+# Transformation of all object columns in lowercase
 columns_to_lower = ['gender', 'Partner', 'Dependents',
         'PhoneService', 'MultipleLines', 'InternetService',
        'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport',
@@ -28,6 +28,6 @@ for c in columns_to_lower:
 
     df[c]=df[c].apply(lambda x: x.lower())
 
-#Save df into a cleaned csv
+# Save df into a cleaned csv
 output_path = os.path.join(data_dir, 'cleaned_datas.csv')
 df.to_csv(output_path, index=False)
