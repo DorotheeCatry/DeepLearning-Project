2ec828aa5e724571bfc14d81d08197818531d0d3
ludivine <ludivine.raby@gmail.com>
2025-05-22 10:08:12 +0200

    Feature : implementation of MLFlow to follow the performance of the models. Addition of the directory path to utils in main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from utils.load import load_data
+from utils.preprocessing import preprocess
+from utils.split import split_data
+
 def main():
     pass
 
